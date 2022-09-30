cdfd041c66932be9ebbbfb144603449f062131ba
DanilSukach <danil211-1@yandex.ru>
2022-09-30 22:25:46 +0400

    датасет с рандомным названием

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import os
 import csv
+import shutil
 import cv2
+import random
+from array import array
 
-def csv_file(path,url):
-    data = [
-        ("absolute_path","relative_path","class"),
-    ]
+def csv_file(path,url,file_name):
+    info = os.listdir(path)
+    data = []
     for i in range(1100):
         n = str(i)
-        absolute = os.path.abspath(path + n.zfill(4) + ".jpg")
-        relative = os.path.relpath(path + n.zfill(4) + ".jpg")
+        absolute = os.path.abspath(path + info[i])
+        relative = os.path.relpath(path + info[i])
         data.append([absolute,relative,url])
-    with open("data.csv","a+",newline="") as file:
+    with open(file_name + ".csv","a+",newline="") as file:
         writer = csv.writer(file,delimiter = ";")
         writer.writerows(data)
 
@@ -19,16 +21,48 @@ def copy(url):
     if not os.path.isdir("dataset_copy"):
         os.mkdir("dataset_copy")
     for i in range(1100):
-        a=str(i)
-        filename=str('dataset/' + url + '/' + a.zfill(4) +'.jpg')
-        image=cv2.imread(filename)
-        filewrite=str('dataset_copy/' + url + '_' + a.zfill(4) +'.jpg')
-        cv2.imwrite(filewrite,image)
+        a = str(i)
+        shutil.copy(
+            os.path.join('dataset/' + url, a.zfill(4) + ".jpg"),
+            os.path.join('dataset_copy/', url + "_" + a.zfill(4) + ".jpg")
+        )
+
+def copy_random(url1,url2):
+    if not os.path.isdir("dataset_copy_random"):
+        os.mkdir("dataset_copy_random")
+    data=[]
+    array=[]
+    for i in range(3000):
+        rand = random.randint(0,10000)
+        if not rand in array:
+            array.append(rand)
+    for i in range(1100):
+        rand = str(array[i])
+        a = str(i)
+        shutil.copy(
+            os.path.join('dataset/' + url1, a.zfill(4) + ".jpg"),
+            os.path.join('dataset_copy_random/', rand + ".jpg")
+        )
+        absolute = os.path.abspath('dataset_copy_random/' + rand + ".jpg")
+        relative = os.path.relpath('dataset_copy_random/' + rand + ".jpg")
+        data.append([absolute,relative,url1])
+        rand1 = str(array[i+1100])
+        shutil.copy(
+            os.path.join('dataset/' + url2, a.zfill(4) + ".jpg"),
+            os.path.join('dataset_copy_random/', rand1 + ".jpg")
+        )
+        absolute = os.path.abspath('dataset_copy_random/' + rand1 + ".jpg")
+        relative = os.path.relpath('dataset_copy_random/' + rand1 + ".jpg")
+        data.append([absolute,relative,url2])
+    with open("data_copy" + ".csv","a+",newline="") as file:
+            writer = csv.writer(file,delimiter = ";")
+            writer.writerows(data)
+    
+        
 
 def main():
-    csv_file("dataset/leopard/","leopard")
-    csv_file("dataset/tiger/","tiger")
-    copy("tiger")
+    copy_random("leopard","tiger")
+    
 
 if __name__ == "__main__":
     main()
